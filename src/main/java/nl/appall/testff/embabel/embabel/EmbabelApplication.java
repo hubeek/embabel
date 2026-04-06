d63f861bd366795f926f837452eeca6b538b2cc3
hjhubeek <hjhubeek@hotmail.com>
2026-04-06 16:15:50 +0200

    init

---
+++ src/main/java/nl/appall/testff/embabel/embabel/EmbabelApplication.java
@@ -0,0 +1,13 @@
+package nl.appall.testff.embabel.embabel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbabelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbabelApplication.class, args);
+    }
+
+}
